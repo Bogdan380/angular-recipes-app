b1d81f0fd39dc2f2b043b9728b6aecd37e5968ff
Bogdan Mitrovic <bogdanmitrovic.lp@gmail.com>
2022-06-21 19:42:22 +0200

    recipe-list template added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'This is just a test',
+      'https://www.tasteofhome.com/wp-content/uploads/2022/06/Thai-Drunken-Noodles-with-Beef_EXPS_RC21_264389_B09_28_4b.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
