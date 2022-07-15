766a7a5aa46f942d663da996b009466f630ea680
Bogdan Mitrovic <bogdanmitrovic.lp@gmail.com>
2022-07-16 00:27:47 +0200

    Added Subject to shopping list

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -16,7 +16,6 @@ export class RecipeEditComponent implements OnInit {
     this.route.params.subscribe((params: Params) => {
       this.id = Number(params['id']);
       this.editMode = params['id'] != null;
-      console.log(this.editMode);
     });
   }
 }
